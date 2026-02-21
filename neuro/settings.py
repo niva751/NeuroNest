09b1b82b5588a24f4d7862dbc3e5792bfafbce40
niva751 <nivasah654324@gmail.com>
2026-02-21 13:52:34 +0545

    Update game.html

--- neuro/settings.py
+++ neuro/settings.py
@@ -118,13 +118,18 @@ USE_TZ = True
 
 ASGI_APPLICATION = 'neuronest.asgi.application'
 
-STATIC_URL = '/static/'
-STATICFILES_DIRS = [BASE_DIR / 'static']
+
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = [BASE_DIR / 'static']
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR / 'media'
+
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
--- neuro/urls.py
+++ neuro/urls.py
@@ -16,10 +16,14 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('core.urls')),   # core app
     path('chat/', include('chat.urls')),   # chat app
     path('games/', include('games.urls')), # games app
-]
+] 
+if settings.DEBUG:
+  urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
