e5106d958f613f65a25384009c6e0220bc100c6e
niva751 <nivasah654324@gmail.com>
2026-02-21 13:33:52 +0545

    update admin.py

--- games/admin.py
+++ games/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Game, GameScore, Drawing, HomeImage
+
+admin.site.register(Game)
+admin.site.register(GameScore)
+admin.site.register(Drawing)
+admin.site.register(HomeImage)
