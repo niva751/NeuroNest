983172c67e413bee36b7b37d1d7fe3157324eae0
niva751 <nivasah654324@gmail.com>
2026-02-21 13:34:24 +0545

    update urls.py
    Updated URL patterns to include a home view and adjusted existing routes.

--- games/urls.py
+++ games/urls.py
@@ -1,10 +1,18 @@
 from django.urls import path
 from . import views
+from django.conf import settings
+from django.conf.urls.static import static
+from django.urls import path
 
 urlpatterns = [
-    path('', views.games_home, name='games_home'),  # <--- new
-      path('flipcard/', views.flipcard, name='flipcard'),
+    path('', views.home, name='home'),  
+    path('game/', views.games_home, name='games_home'),
+    path('flipcard/', views.flipcard, name='flipcard'),
     path('sudoku/', views.sudoku, name='sudoku'),
     path('hangman/', views.hangman, name='hangman'),
+    path('math_quiz/', views.math_quiz, name='math_quiz'),
     path('draw/', views.draw_page, name='draw_page'),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
